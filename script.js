1b60f8d27fb8e0b53c2fba2b4aa9da02634134b5
D. Wilson <34779401+dwilson-coder@users.noreply.github.com>
2020-04-02 17:23:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function openNav(){
+  document.getElementById("side-menu").style.width = "300px";
+  document.getElementById("content-area").style.marginLeft = "300px";
+}
+
+
+function closeNav(){
+  document.getElementById("side-menu").style.width = "0";
+  document.getElementById("content-area").style.marginLeft = "0";
+}
